8e6757ee636920f3f7640c9af7f7b616f15e77ce
dmvict <dm.vict.kr@gmail.com>
2021-11-17 09:18:21 +0200

    Fix test routine `gitIdentityScript`, use no commands of utility `Censor`

--- proto/wtools/atop/identity.test/Ext.test.s
+++ proto/wtools/atop/identity.test/Ext.test.s
@@ -695,7 +695,7 @@ module.exports = onIdentity;
     test.identical( _.strCount( op.output, 'module.exports = onIdentity;' ), 0 );
     return null;
   });
-  a.appStart( `.profile.del profile:${profile}` );
+  a.ready.finally( () => { _.censor.profileDel( profile ); return null });
 
   /* */
 
@@ -714,7 +714,7 @@ module.exports = onIdentity;
     test.identical( _.strCount( op.output, script ), 1 );
     return null;
   });
-  a.appStart( `.profile.del profile:${profile}` );
+  a.ready.finally( () => { _.censor.profileDel( profile ); return null });
 
   /* - */
 
